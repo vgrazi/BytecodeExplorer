1a54ea63f59e305ec29b6aa4c4829c4adc867139
Victor J Grazi <vgrazi@gmail.com>
2016-01-02 23:36:00 -0500

    Removed specious fields

--- src/main/java/com/vgrazi/bytecodeexplorer/structure/constantTypes/ConstantFloat.java
+++ src/main/java/com/vgrazi/bytecodeexplorer/structure/constantTypes/ConstantFloat.java
@@ -4,19 +4,16 @@ import com.vgrazi.bytecodeexplorer.utils.IEEE754Converter;
 import com.vgrazi.bytecodeexplorer.utils.Utils;
 
 /**
- * Created by vgrazi on 8/13/15.
+ * Created by vgrazi on 8/13/15
  */
 public class ConstantFloat extends ConstantType {
     private int startByteIndex;
-    private int classIndex;
-    private int nameAndTypeIndex;
-    private byte[] bytes;
     private String explanation;
 
     /**
      * "tag item" is how the documentation refers to the type byte
      *
-     * @return
+     * @return 4
      */
     @Override
     public byte getTag() {
@@ -26,10 +23,6 @@ public class ConstantFloat extends ConstantType {
     @Override
     public void setData(byte[] bytes, int index) {
         this.startByteIndex = index;
-        this.classIndex = Utils.getTwoBytes(bytes, index + 1);
-        this.nameAndTypeIndex = Utils.getTwoBytes(bytes, index + 3);
-
-        this.bytes = bytes;
         int IEE754 = Utils.getFourBytes(bytes, index + 1);
         double value = IEEE754Converter.convertToDecimal(IEE754);
         byte signBit = IEEE754Converter.extractSignBit(IEE754);
@@ -38,7 +31,6 @@ public class ConstantFloat extends ConstantType {
         long mantissaBytes = IEEE754Converter.extractMantissaBytes(IEE754);
         explanation = getFormattedAddressAndConstantIndex() + " ConstantFloat<br/>" +
             IEEE754Converter.getExplanation(IEE754, value, signBit, exponent, mantissa, mantissaBytes);
-        // todo: continue here
     }
 
     @Override
@@ -59,7 +51,7 @@ public class ConstantFloat extends ConstantType {
     /**
      * Index to the first byte of this section relative to the constant pool
      *
-     * @return
+     * @return the start byte index for this section
      */
     @Override
     public int getStartByteIndex() {
